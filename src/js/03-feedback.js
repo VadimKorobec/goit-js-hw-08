b68ddf8ecad2553c079fc77d47b5ff8d96e5e6be
VadimKorobec <_s_e_t__@ukr.net>
2022-11-06 19:22:39 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,27 +8,9 @@ initForm();
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
 
-function onFormSubmit(e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  formData.forEach((value, name) => console.log(value, name));
-  e.currentTarget.reset();
-  localStorage.removeItem(feedbackForm);
-}
+function onFormSubmit(event) {}
 
-function onFormInput(e) {
-  let persistedFilters = localStorage.getItem(feedbackForm);
-  persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
-  persistedFilters[e.target.name] = e.target.value;
-  localStorage.setItem(feedbackForm, JSON.stringify(persistedFilters));
-}
+function onFormInput(event) {}
 
-function initForm() {
-  let persistedFilters = localStorage.getItem(feedbackForm);
-  if (persistedFilters) {
-    persistedFilters = JSON.parse(persistedFilters);
-    Object.entries(persistedFilters).forEach(([name, value]) => {
-      form.elements[name].value = value;
-    });
-  }
-}
+function initForm() {}
+console.log();
