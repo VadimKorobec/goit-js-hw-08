33031890211eb49ed41da8c80bf75858656fdfd3
VadimKorobec <_s_e_t__@ukr.net>
2022-11-06 18:34:17 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,3 +2,21 @@ import throttle from 'lodash.throttle';
 
 const feedbackForm = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
+
+initForm()
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+}
+
+function onFormInput(event) {
+  const message = event.target.value;
+  console.log(message);
+  localStorage.setItem(feedbackForm, message);
+}
+
+function initForm()
