bb8a02e36d369bd5e43929cc2c21615b49a2de47
VadimKorobec <_s_e_t__@ukr.net>
2022-11-06 17:24:38 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galaryListRef = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(img => {
+    return `<div class="gallery__item">
+  <a class="gallery__item" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+galaryListRef.innerHTML = markup;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionsDelay: 250,
+});
